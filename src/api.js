8867da46467aff1cef69e0234e057c473054f4ed
LavanyaCS <lavanyachandranece@gmail.com>
2025-09-28 21:13:13 +0530

    order

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-// export const baseUrl = "https://ecommerce-backend-8hzj.onrender.com";
-// console.log("BASE URL ===>", baseUrl);
-
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://ecommerce-backend-8hzj.onrender.com";
 console.log("BASE URL ===>", baseUrl);
+
+// export const baseUrl = "http://localhost:5000";
+// console.log("BASE URL ===>", baseUrl);
 
 
 
