c6d63cf5f1d7e3c48badc2ba79ce4c53b51e2c5c
LavanyaCS <lavanyachandranece@gmail.com>
2025-10-06 14:18:28 +0530

    found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+    <div className="flex flex-col items-center justify-center h-full bg-gray-100">
+      <h1 className="text-6xl font-bold text-gray-900 mb-4">404</h1>
       <p className="text-xl mb-4">Page Not Found</p>
-      <Link to="/" className="text-blue-500 underline">
+      <Link to="/" className="bg-gray-900 p-2 text-white rounded">
         Go Back Home
       </Link>
     </div>
